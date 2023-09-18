145ab358c8821553743d0d11ab3cf86782a4eea5
Argelbargel <karsten.kraus@web.de>
2023-09-19 00:20:57 +0200

    add tests for azure-auth

--- internal/app/vault_raft_snapshot_agent/vault/auth/azure_test.go
+++ internal/app/vault_raft_snapshot_agent/vault/auth/azure_test.go
@@ -1,11 +1,17 @@
 package auth
 
-/*
-func TestCreateAzureAuth(t *testing.C) {
+import (
+	"testing"
+
+	"github.com/hashicorp/vault/api/auth/azure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAzureAuth(t *testing.T) {
 	config := AzureAuthConfig{
-		Role: "test-role",
+		Role:     "test-role",
 		Resource: "test-resource",
-		Path: "test-path",
+		Path:     "test-path",
 	}
 
 	expectedAuthMethod, err := azure.NewAzureAuth(
@@ -15,9 +21,8 @@ func TestCreateAzureAuth(t *testing.C) {
 	)
 	assert.NoError(t, err, "NewAzureAuth failed unexpectedly")
 
-	VaultAuth, err := createAzureAuth(config)
-	assert.NoError(t, err, "createAzureAuth failed unexpectedly")
+	authMethod, err := createAzureAuth(config).createAuthMethod()
+	assert.NoError(t, err, "createAuthMethod failed unexpectedly")
 
-	assert.Equal(t, expectedAuthMethod, VaultAuth.delegate)
+	assert.Equal(t, expectedAuthMethod, authMethod)
 }
-*/
